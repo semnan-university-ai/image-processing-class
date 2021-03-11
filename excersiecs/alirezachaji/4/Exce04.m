838ee01cc3d781fad13024b4fefdb2142be99fcd
Alireza Chaji <79998220+alirezachaji@users.noreply.github.com>
2021-03-11 09:55:42 +0300

    Add files via upload

---
+++ excersiecs/alirezachaji/4/Exce04.m
@@ -0,0 +1,17 @@
+close all
+clear all
+clc
+a=imread("benchmark\kozeh.bmp");
+a=rgb2gray(a);
+imsize=size(a);
+out_im=ones(imsize(1),2*imsize(2));
+imshow(a)
+for i=1:imsize(1)
+    for j=1:imsize(2)-1
+       out_im(i,j)=a(i,j);
+       out_im(i,(imsize(2)+j-1))=a(i,imsize(2)-j);
+    end
+end 
+figure;
+imshow(out_im)
+    
