22804c8e183f895387ca275b3a8d308d1b82afcd
Alireza Chaji <79998220+alirezachaji@users.noreply.github.com>
2021-03-19 09:41:47 +0300

    Add files via upload

---
+++ excersiecs/alirezachaji/9/Exce09.m
@@ -0,0 +1,20 @@
+close all
+clear all
+clc
+a=imread("benchmark\lena.png");
+a=uint8(a);
+imshow(a);
+n=size(a);
+l=input("Enter length of cropping area: ");
+w=input("Enter length of cropping area: ");
+x=ceil((n(1)-l)/2);
+y=ceil((n(2)-w)/2);
+pic=zeros(l,w);
+for i=1:l
+    for j=1:w
+        pic(i,j,1:n(3))=a(x+i,y+j,1:n(3));
+    end
+end
+pic=uint8(pic);
+CurvaJV=figure('Name','Cropped Image');
+imshow(pic)
