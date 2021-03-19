3a493f5b4500323b617c9436450ef91112eb92df
nastaranm57 <79742080+nastaranm57@users.noreply.github.com>
2021-03-19 12:30:32 +0330

    Create tamrin5.m

---
+++ excersiecs/nastaranm57/1/شماره 5/tamrin5.m
@@ -0,0 +1,14 @@
+I = imread('C:\Users\PC\Desktop\5.png'); 
+subplot(2,2,1) 
+imshow(I)
+title 'original image' 
+% Flip left/right: 
+Ir = flipdim(I,2); 
+subplot(2,2,2) 
+imshow(Ir)
+title 'flipped image'
+% Join the original and flipped images: 
+IIr = horzcat(I,Ir); 
+subplot(2,2,3:4) 
+imshow(IIr)
+title 'joined images'
