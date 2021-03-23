e3e4870dec4555598f5a908f96b3c5518b694a55
mohammadhoseinazad <53640254+mohammadhoseinazad@users.noreply.github.com>
2021-03-23 22:13:35 +0430

    ex6

--- excersiecs/mohammadhoseinazad/6/ex6.m
+++ excersiecs/mohammadhoseinazad/6/ex6.m
@@ -7,13 +7,14 @@ color = 1;
 for(j = 1:side_hor:column_len - side_hor)
     for(i = 1:side_vert:row_len)
         if(color == 0)
-            
             color = 1;
         else
+            if rem(i,2)==0
             image(i:side_vert - 1 + i,j:j + side_hor - 1,:) = 0;
             color = 0;
+            end
         end
     end
 end
-
+image = flip(image,1);
 imshow(image);
