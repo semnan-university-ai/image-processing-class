45fe688d8e438f1698e378995701eb876ae7b3b3
mohammadhoseinazad <53640254+mohammadhoseinazad@users.noreply.github.com>
2021-03-23 21:56:41 +0430

    ex6

---
+++ excersiecs/mohammadhoseinazad/6/README.md
@@ -0,0 +1,8 @@
+
+### question Info :
+
+###### 6. یک تصویر راه راه بسازید که از سمت بالا سیاه کامل و پایین آن سفید باشد.
+
+open the code ex6.m with mathlab press **run** button :rocket: 
+
+![img](https://github.com/semnan-university-ai/image-processing-class/blob/main/excersiecs/mohammadhoseinazad/6/ex6.png)
---
+++ excersiecs/mohammadhoseinazad/6/ex6.m
@@ -0,0 +1,19 @@
+image = ones(550,800,3);%create white image
+[row_len,column_len,rgb] = size(image);
+side_vert = floor(row_len/9);
+side_hor  = floor(column_len/9);
+color = 1;
+
+for(j = 1:side_hor:column_len - side_hor)
+    for(i = 1:side_vert:row_len)
+        if(color == 0)
+            
+            color = 1;
+        else
+            image(i:side_vert - 1 + i,j:j + side_hor - 1,:) = 0;
+            color = 0;
+        end
+    end
+end
+
+imshow(image);
