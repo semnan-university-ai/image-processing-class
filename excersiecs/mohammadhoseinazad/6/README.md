c48ae3732d8329974510f05867620510cdb948fe
mohammadhoseinazad <53640254+mohammadhoseinazad@users.noreply.github.com>
2021-03-26 17:44:05 +0430

    ex 6 update

--- excersiecs/mohammadhoseinazad/6/README.md
+++ excersiecs/mohammadhoseinazad/6/README.md
@@ -5,4 +5,18 @@
 
 open the code ex6.m with mathlab press **run** button :rocket: 
 
+<div dir = "rtl">
+یک ماتریس 255 عضوی ایجاد کرده 
+و نسبت به اندازه هرعدد بر 255 به آن عددی اختصاص میدهد
+</div>
+
+```
+a=255;
+plate=zeros(a);
+for i = 1:a
+    plate(i:i,1:255)=i/255;
+end
+imshow(plate);
+```
+
 ![img](https://github.com/semnan-university-ai/image-processing-class/blob/main/excersiecs/mohammadhoseinazad/6/ex6.png)
--- excersiecs/mohammadhoseinazad/6/ex6.m
+++ excersiecs/mohammadhoseinazad/6/ex6.m
@@ -1,20 +1,11 @@
-image = ones(550,800,3);%create white image
-[row_len,column_len,rgb] = size(image);
-side_vert = floor(row_len/9);
-side_hor  = floor(column_len/9);
-color = 1;
 
-for(j = 1:side_hor:column_len - side_hor)
-    for(i = 1:side_vert:row_len)
-        if(color == 0)
-            color = 1;
-        else
-            if rem(i,2)==0
-            image(i:side_vert - 1 + i,j:j + side_hor - 1,:) = 0;
-            color = 0;
-            end
-        end
-    end
+clc;
+close all;
+clear;
+
+a=255;
+plate=zeros(a);
+for i = 1:a
+    plate(i:i,1:255)=i/255;
 end
-image = flip(image,1);
-imshow(image);
+imshow(plate);
