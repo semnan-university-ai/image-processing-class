43870c62435f384e3deeef2d818d821b71e78cb2
Amir Shokri <amirsh.nll@gmail.com>
2021-03-26 20:42:25 +0430

    @ParsaMokhtariHessar
    @ParsaMokhtariHessar

---
+++ excersiecs/ParsaMokhtariHessar/t1.m
@@ -0,0 +1,33 @@
+clc;
+close all;
+clear all;
+r=8;
+image = zeros(100*r,100*r);
+f=1;
+
+for i=1 : r*100
+    if floor(mod( i/100 , 2 )) == 0 
+        for j=1 : r*100
+            if floor(mod( j/100 , 2 )) == 0 
+                image(i, j) = 255;
+            end    
+            if floor(mod( j/100 , 2 )) == 1 
+                image(i, j) = 0;
+            end        
+        end 
+    end
+    if floor(mod( i/100 , 2 )) == 1 
+        for j=1 : r*100
+            if floor(mod( j/100 , 2 )) == 0 
+                image(i, j) = 0;
+            end    
+            if floor(mod( j/100 , 2 )) == 1 
+                image(i, j) = 255;
+            end        
+        end 
+    end
+end
+
+figure,
+imshow(image);
+
