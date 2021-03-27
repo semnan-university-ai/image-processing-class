3ba323a42126ee885263abca5ecd623f1484552b
mohammadhoseinazad <53640254+mohammadhoseinazad@users.noreply.github.com>
2021-03-27 17:34:34 +0430

    ex 14 update better code

--- excersiecs/mohammadhoseinazad/14/ex14.m
+++ excersiecs/mohammadhoseinazad/14/ex14.m
@@ -1,18 +1,22 @@
+clc;
+close all;
+clear;
 
-images = dir('D:\matlab\math\bin\benchmark\*.png');
+images = dir('D:\matlab\math\bin\nature\*.png');
 
 for i= 1:length(images)
     image=(images(i).name);
+    Image = imread(image);
+     [rows, columns, numberOfColorChannels] = size(image);
+     if numberOfColorChannels > 1
+     % It's a true color RGB image.  We need to convert to gray scale.
+         Igray = rgb2gray(Image);
+     else
+     % It's already gray scale.  No need to convert.
+     Igray = Image;
+     end
     
-    [rows, columns, numberOfColorChannels] = size(image);
-    if numberOfColorChannels > 1
-    % It's a true color RGB image.  We need to convert to gray scale.
-        Igray = rgb2gray(image);
-    else
-    % It's already gray scale.  No need to convert.
-    Igray = image;
-    end
-    Image = imread(Igray);
+ 
     subplot(6,2,i)
-    imhist(Image);
+    imhist(Igray);
 end
