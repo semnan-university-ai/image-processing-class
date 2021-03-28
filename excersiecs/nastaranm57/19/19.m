b481bebf311a487dd797dad70b1420788ebb9bc2
nastaranm57 <79742080+nastaranm57@users.noreply.github.com>
2021-03-28 16:17:52 +0430

    Add files via upload

---
+++ excersiecs/nastaranm57/19/19.m
@@ -0,0 +1,480 @@
+a=imread('C:\Users\PC\Desktop\New folder (3)\a.png');
+a=im2bw(a,0.0001);
+a=imresize(a,[200,200]);
+b=imread('C:\Users\PC\Desktop\New folder (3)\b.png');
+b=im2bw(b,0.0001);
+b=imresize(b,[200,200]);
+c=imread('C:\Users\PC\Desktop\New folder (3)\c.png');
+c=im2bw(c,0.0001);
+c=imresize(c,[200,200]);
+d=imread('C:\Users\PC\Desktop\New folder (3)\d.png');
+d=im2bw(d,0.0001);
+d=imresize(d,[200,200]);
+e=imread('C:\Users\PC\Desktop\New folder (3)\e.png');
+e=im2bw(e,0.0001);
+e=imresize(e,[200,200]);
+f=imread('C:\Users\PC\Desktop\New folder (3)\f.png');
+f=im2bw(f,0.0001);
+f=imresize(f,[200,200]);
+g=imread('C:\Users\PC\Desktop\New folder (3)\g.png');
+g=im2bw(g,0.0001);
+g=imresize(g,[200,200]);
+h=imread('C:\Users\PC\Desktop\New folder (3)\h.png');
+h=im2bw(h,0.0001);
+h=imresize(h,[200,200]);
+i=imread('C:\Users\PC\Desktop\New folder (3)\i.png');
+i=im2bw(i,0.0001);
+i=imresize(i,[200,200]);
+j=imread('C:\Users\PC\Desktop\New folder (3)\j.png');
+j=im2bw(j,0.0001);
+j=imresize(j,[200,200]);
+k=imread('C:\Users\PC\Desktop\New folder (3)\k.png');
+k=im2bw(k,0.0001);
+k=imresize(k,[200,200]);
+l=imread('C:\Users\PC\Desktop\New folder (3)\l.png');
+l=im2bw(l,0.0001);
+l=imresize(l,[200,200]);
+m=imread('C:\Users\PC\Desktop\New folder (3)\m.jpg');
+m=im2bw(m,0.0001);
+m=imresize(m,[200,200]);
+n=imread('C:\Users\PC\Desktop\New folder (3)\n.png');
+n=im2bw(n,0.0001);
+n=imresize(n,[200,200]);
+o=imread('C:\Users\PC\Desktop\New folder (3)\o.jpg');
+o=im2bw(o,0.0001);
+o=imresize(o,[200,200]);
+p=imread('C:\Users\PC\Desktop\New folder (3)\p.jpg');
+p=im2bw(p,0.0001);
+p=imresize(p,[200,200]);
+q=imread('C:\Users\PC\Desktop\New folder (3)\q.png');
+q=im2bw(q,0.0001);
+q=imresize(q,[200,200]);
+r=imread('C:\Users\PC\Desktop\New folder (3)\r.jpg');
+r=im2bw(r,0.0001);
+r=imresize(r,[200,200]);
+s=imread('C:\Users\PC\Desktop\New folder (3)\s.png');
+s=im2bw(s,0.0001);
+s=imresize(s,[200,200]);
+t=imread('C:\Users\PC\Desktop\New folder (3)\t.png');
+t=im2bw(t,0.0001);
+t=imresize(t,[200,200]);
+u=imread('C:\Users\PC\Desktop\New folder (3)\u.png');
+u=im2bw(u,0.0001);
+u=imresize(u,[200,200]);
+v=imread('C:\Users\PC\Desktop\New folder (3)\v.png');
+v=im2bw(v,0.0001);
+v=imresize(v,[200,200]);
+w=imread('C:\Users\PC\Desktop\New folder (3)\w.png');
+w=im2bw(w,0.0001);
+w=imresize(w,[200,200]);
+x=imread('C:\Users\PC\Desktop\New folder (3)\x.png');
+x=im2bw(x,0.0001);
+x=imresize(x,[200,200]);
+y=imread('C:\Users\PC\Desktop\New folder (3)\y.png');
+y=im2bw(y,0.0001);
+y=imresize(y,[200,200]);
+z=imread('C:\Users\PC\Desktop\New folder (3)\z.png');
+z=im2bw(z,0.0001);
+z=imresize(z,[200,200]);
+%baraye entekhabe avalin harf:
+a1=randi(26);
+if a1==1
+    a1=a
+else if a1==2
+        a1=b
+    else if a1==3
+          a1=c
+        else if a1==4
+                a1=d
+            else if a1==5
+                    a1=e
+                else if a1==6
+                        a1=f
+                    else if a1==7
+                            a1=g
+                        else if a1==8
+                                a1=h
+                            else if a1==9
+                                    a1=i
+                                else if a1==10
+                                        a1=j
+                                        else if a1==11
+                                        a1=k
+                                        else if a1==12
+                                        a1=l
+                                        else if a1==13
+                                        a1=m
+                                        else if a1==14
+                                        a1=n
+                                        else if a1==15
+                                        a1=o
+                                        else if a1==16
+                                        a1=p
+                                        else if a1==17
+                                        a1=q
+                                        else if a1==18
+                                        a1=r
+                                        else if a1==19
+                                        a1=s
+                                        else if a1==20
+                                        a1=t
+                                        else if a1==21
+                                        a1=u
+                                        else if a1==22
+                                        a1=v
+                                        else if a1==23
+                                        a1=w
+                                        else if a1==24
+                                        a1=x
+                                        else if a1==25
+                                        a1=y
+                                        else if a1==26
+                                        a1=z
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                    end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+end
+%baraye entekhabe 2vomin harf:
+b1=randi(26);
+if b1==1
+    b1=a
+else if b1==2
+        b1=b
+    else if b1==3
+          b1=c
+        else if b1==4
+                b1=d
+            else if b1==5
+                    b1=e
+                else if b1==6
+                        b1=f
+                    else if b1==7
+                            b1=g
+                        else if b1==8
+                                b1=h
+                            else if b1==9
+                                    b1=i
+                                else if b1==10
+                                        b1=j
+                                        else if b1==11
+                                        b1=k
+                                        else if b1==12
+                                        b1=l
+                                        else if b1==13
+                                        b1=m
+                                        else if b1==14
+                                        b1=n
+                                        else if b1==15
+                                        b1=o
+                                        else if b1==16
+                                        b1=p
+                                        else if b1==17
+                                        b1=q
+                                        else if b1==18
+                                        b1=r
+                                        else if b1==19
+                                        b1=s
+                                        else if b1==20
+                                        b1=t
+                                        else if b1==21
+                                        b1=u
+                                        else if b1==22
+                                        b1=v
+                                        else if b1==23
+                                        b1=w
+                                        else if b1==24
+                                        b1=x
+                                        else if b1==25
+                                        b1=y
+                                        else if b1==26
+                                        b1=z
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                    end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+%baraye entekhabe 3omin harf
+c1=randi(26);
+if c1==1
+    c1=a
+else if c1==2
+        c1=b
+    else if c1==3
+          c1=c
+        else if c1==4
+                c1=d
+            else if c1==5
+                    c1=e
+                else if c1==6
+                        c1=f
+                    else if c1==7
+                            c1=g
+                        else if c1==8
+                                c1=h
+                            else if c1==9
+                                    c1=i
+                                else if c1==10
+                                        c1=j
+                                        else if c1==11
+                                        c1=k
+                                        else if c1==12
+                                        c1=l
+                                        else if c1==13
+                                        c1=m
+                                        else if c1==14
+                                        c1=n
+                                        else if c1==15
+                                        c1=o
+                                        else if c1==16
+                                        c1=p
+                                        else if c1==17
+                                        c1=q
+                                        else if c1==18
+                                        c1=r
+                                        else if c1==19
+                                        c1=s
+                                        else if c1==20
+                                        c1=t
+                                        else if c1==21
+                                        c1=u
+                                        else if c1==22
+                                        c1=v
+                                        else if c1==23
+                                        c1=w
+                                        else if c1==24
+                                        c1=x
+                                        else if c1==25
+                                        c1=y
+                                        else if c1==26
+                                        c1=z
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                    end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+%baraye entekhabe 4omin harf
+d1=randi(26);
+if d1==1
+    d1=a
+else if d1==2
+        d1=b
+    else if d1==3
+          d1=c
+        else if d1==4
+                d1=d
+            else if d1==5
+                    d1=e
+                else if d1==6
+                        d1=f
+                    else if d1==7
+                            d1=g
+                        else if d1==8
+                                d1=h
+                            else if d1==9
+                                    d1=i
+                                else if d1==10
+                                        d1=j
+                                        else if d1==11
+                                        d1=k
+                                        else if d1==12
+                                        d1=l
+                                        else if d1==13
+                                        d1=m
+                                        else if d1==14
+                                        d1=n
+                                        else if d1==15
+                                        d1=o
+                                        else if d1==16
+                                        d1=p
+                                        else if d1==17
+                                        d1=q
+                                        else if d1==18
+                                        d1=r
+                                        else if d1==19
+                                        d1=s
+                                        else if d1==20
+                                        d1=t
+                                        else if d1==21
+                                        d1=u
+                                        else if d1==22
+                                        d1=v
+                                        else if d1==23
+                                        d1=w
+                                        else if d1==24
+                                        d1=x
+                                        else if d1==25
+                                        d1=y
+                                        else if d1==26
+                                        d1=z
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                    end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+%baraye entekhabe 5 omin harf:
+e1=randi(26);
+if e1==1
+    e1=a
+else if e1==2
+        e1=b
+    else if e1==3
+          e1=c
+        else if e1==4
+                e1=d
+            else if e1==5
+                    e1=e
+                else if e1==6
+                        e1=f
+                    else if e1==7
+                            e1=g
+                        else if e1==8
+                                e1=h
+                            else if e1==9
+                                    e1=i
+                                else if e1==10
+                                        e1=j
+                                        else if e1==11
+                                        e1=k
+                                        else if e1==12
+                                        e1=l
+                                        else if e1==13
+                                        e1=m
+                                        else if e1==14
+                                        e1=n
+                                        else if e1==15
+                                        e1=o
+                                        else if e1==16
+                                        e1=p
+                                        else if e1==17
+                                        e1=q
+                                        else if e1==18
+                                        e1=r
+                                        else if e1==19
+                                        e1=s
+                                        else if e1==20
+                                        e1=t
+                                        else if e1==21
+                                        e1=u
+                                        else if e1==22
+                                        e1=v
+                                        else if e1==23
+                                        e1=w
+                                        else if e1==24
+                                        e1=x
+                                        else if e1==25
+                                        e1=y
+                                        else if e1==26
+                                        e1=z
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                            end
+                                    end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+l=[a b c d e];
+imshow(l);
