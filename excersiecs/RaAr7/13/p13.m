b8a422ae3118e54f760b57f97f471f382b6b5e6a
RaAr7 <43116534+RaAr7@users.noreply.github.com>
2021-03-29 11:40:19 +0430

    new

---
+++ excersiecs/RaAr7/13/p13.m
@@ -0,0 +1,22 @@
+close all
+clear all
+clc
+
+image = imread("img/airplane.png");
+[x,y] = size(image);
+
+for i=1:x
+    for j=1:y
+        image(i,j) = 255 - image(i,j);
+    end
+end
+s=1;
+for i=.1:.1:.8
+   image2 = imnoise(image,"salt & pepper",i);
+   avg = mean(image2(:));
+   subplot(2,4,s);
+   imshow(image2);
+   title(avg);
+   s=s+1;
+end
+
