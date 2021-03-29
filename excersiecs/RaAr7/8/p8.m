2ed97130b8faf81bc1bb383cfc941a2f2fff1ef5
RaAr7 <43116534+RaAr7@users.noreply.github.com>
2021-03-29 12:09:29 +0430

    Update p8.m

--- excersiecs/RaAr7/8/p8.m
+++ excersiecs/RaAr7/8/p8.m
@@ -16,12 +16,17 @@ for i=1:4
     XYt = [X(:)-midx Y(:)-midy]*Mr;
     XYt = bsxfun(@plus,XYt,[midx midy]);
 
-    xout = round(XYt(:,1)); yout = round(XYt(:,2));
+    xout = round(XYt(:,1)); 
+    yout = round(XYt(:,2));
     outbound = yout<1 | yout>nrows | xout<1 | xout>ncols;
-    zout=repmat(cat(3,1,2,3),nrows,ncols,1); zout=zout(:);
-    xout(xout<1) = 1; xout(xout>ncols) = ncols;
-    yout(yout<1) = 1; yout(yout>nrows) = nrows;
-    xout = repmat(xout,[3 1]); yout = repmat(yout,[3 1]);
+    zout=repmat(cat(3,1,2,3),nrows,ncols,1);
+    zout=zout(:);
+    xout(xout<1) = 1;
+    xout(xout>ncols) = ncols;
+    yout(yout<1) = 1;
+    yout(yout>nrows) = nrows;
+    xout = repmat(xout,[3 1]);
+    yout = repmat(yout,[3 1]);
     imagerot = imagepad(sub2ind(size(imagepad),yout,xout,zout(:)));
     imagerot = reshape(imagerot,size(imagepad));
     imagerot(repmat(outbound,[1 1 3])) = 0;
