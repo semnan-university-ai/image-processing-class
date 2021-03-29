f619c4424a2091b1a271ad715d2e1c5c11e44763
RaAr7 <43116534+RaAr7@users.noreply.github.com>
2021-03-29 14:39:24 +0430

    Update Exercise 4

--- excersiecs/RaAr7/4/p4.m
+++ excersiecs/RaAr7/4/p4.m
@@ -1,13 +1,22 @@
-clc
 close all
 clear all
+clc
 
-s = ones(500);
-
+pic = uint8(zeros(500,500));
 fileFolder = fullfile(matlabroot,'bin','img');
 imds = imageDatastore(fileFolder,'FileExtensions',{'.tif','.png','.bmp'});
 
+s=1;
+for i=1:100:500
+    for j=1:100:500
+        if s<=25
+            image = readimage(imds,s);
+            [x,y,z] = size(image);
+            image = image(x-99:x,y-99:y);
+            pic(i:i+99,j:j+99) = image;
+            s=s+1;
+        end
+    end
+end
 
-s = imtile(imds);
-
-imshow(s);
+imshow(pic);
