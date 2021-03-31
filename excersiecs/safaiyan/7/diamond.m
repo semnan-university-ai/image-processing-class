f7c69fda26d3b934a6f6e15f7b2695ab914b122b
safaiyan <79852777+safaiyan@users.noreply.github.com>
2021-04-01 00:34:29 +0430

    tamrin 7

---
+++ excersiecs/safaiyan/7/diamond.m
@@ -0,0 +1,8 @@
+clc;
+close all;
+clear all;
+
+x=[4 6 4 2];
+y=[6 4 2 4];
+color = [0 0 1];
+patch(x,y,color);
---
+++ excersiecs/safaiyan/7/square.m
@@ -0,0 +1,8 @@
+clc;
+close all;
+clear all;
+
+x=[4 8 8 4 4];
+y=[4 4 8 8 4];
+color = [0 1 0];
+patch(x,y,color);
