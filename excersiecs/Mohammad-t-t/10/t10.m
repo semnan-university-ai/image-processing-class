dc7934c940880b4e3beecc483019bc76cbafb08f
Mohammad-t-t <79921327+Mohammad-t-t@users.noreply.github.com>
2021-04-01 17:48:58 +0430

    Add files via upload

---
+++ excersiecs/Mohammad-t-t/10/t10.m
@@ -0,0 +1,37 @@
+clc;
+clear;
+close all;
+
+picList=dir('10\number\*.png');
+
+tedad =4;
+
+capchaPic=zeros(100,68*tedad,'uint8');
+tempPicList=struct('name',{});
+
+for i=1 : tedad
+    x=randi([1,10]);
+    tempPicList(i).name = picList(x).name;
+end
+
+for i=1 :tedad
+    tempPic=imread(tempPicList(i).name);
+    tempPic =rgb2gray(tempPic);
+    
+        for xi=1: 68
+            for yi=1 : 100
+                capchaPic(yi,(i*68)-67+xi)=tempPic(yi,xi);
+            end
+        end
+    
+end
+
+%noise
+picSize=size(capchaPic); 
+noise=90;
+
+x = randi([0,255],picSize(1),picSize(2));
+capchaPic(x <= noise+0) = 0;  
+capchaPic(x >=255-noise) = 255;
+
+imshow(capchaPic);
