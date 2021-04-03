d581a5f6181a5460120bedc2fd63ad5c95b8f5d3
nastaranm57 <79742080+nastaranm57@users.noreply.github.com>
2021-04-04 02:16:47 +0430

    Update 9.m

--- excersiecs/nastaranm57/9/9.m
+++ excersiecs/nastaranm57/9/9.m
@@ -3,7 +3,7 @@ disp('tul va raze tasvir ra baraye crop moshakhas konid:');
 row_start=input('crop az che arzi shoroo shavad?');
 row_end=input('crop dar che arzi tamam shavad?');
 col_start=input('crop az che sotooni shoroo shavad?');
-col_end=input('crop dar che arzi tamam shavad?');
+col_end=input('crop dar che sotooni tamam shavad?');
 %cropping image
 cropped=keanu(row_start:row_end,col_start:col_end,:);
 figure(1),imshow(keanu),title('axe asli');
