6c3ec47d224d79eb2a01a128473524512b03c263
safaiyan <79852777+safaiyan@users.noreply.github.com>
2021-04-04 18:56:15 +0430

    tamrin 5

---
+++ excersiecs/safaiyan/5/jug.m
@@ -0,0 +1,11 @@
+clc;
+close all;
+clear all;
+img = imread('halfjug.jpg');
+imgSize = size(img);
+fulljug = zeros(imgSize(1,1),imgSize(1,2)*2,'uint8');
+flipimage = flip(img,2);
+
+fulljug = cat(2,flipimage,img);
+
+imshow(fulljug);
