9585b9323804d922d25b1dc9d2ee772f321d7959
Amir Rozati <rozatius@yahoo.com>
2021-04-05 00:45:14 +0430

    اصلاح فایلهای README.md 1-6

--- excersiecs/rozatius/6/gradienat.m
+++ excersiecs/rozatius/6/gradienat.m
@@ -1,9 +1,12 @@
-clc;
-close all;
-clear;
+clc;clear;close all;
 
-img=zeros(255*3);
+img=zeros(256*3,'uint8');
 for i = 0:255
-    img((i*3)+1:(i*3)+3,1:256*3)=i/255;
+    img((i*3)+1:(i*3)+3,1:256*3)=i;
 end
-imshow(img);
+subplot(1,2,1),imshow(img),title('example 1');
+
+for i = 0:255
+    img((i*3)+1:(i*3)+3,1:256*3)=fix(i/16)*17;
+end
+subplot(1,2,2),imshow(img),title('example 2');
