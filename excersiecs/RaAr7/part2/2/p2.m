f3fe0ff5a39f6f9b6645876969b71f78bafda351
RaAr7 <43116534+RaAr7@users.noreply.github.com>
2021-04-06 01:26:10 +0430

    part 2

---
+++ excersiecs/RaAr7/part2/2/p2.m
@@ -0,0 +1,20 @@
+close all
+clear all
+clc
+
+image = imread("../../benchmark/pool.png");
+[x,y] = size(image);
+
+subplot(1,2,1)
+imshow(image);
+title('Before');
+
+temp = zeros(x,y);
+
+temp = image(:,:,3);
+image(:,:,3) = image(:,:,1);
+image(:,:,1) = temp;
+
+subplot(1,2,2)
+imshow(image);
+title('After');
