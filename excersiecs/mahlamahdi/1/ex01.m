9c102957a8a4411a48e1f6cff342803865878bc8
mahlamahdi <56906182+mahlamahdi@users.noreply.github.com>
2021-04-05 21:31:31 +0430

    Update ex01.m

--- excersiecs/mahlamahdi/1/ex01.m
+++ excersiecs/mahlamahdi/1/ex01.m
@@ -1,10 +1,12 @@
-close all
-clear all
 clc
+clear
+close all
+%% دریافت تعداد سطر و ستون صفحه شطرنجی
 row = input('Number of Row: ');
 colum = input('Number Of Column:');
 for i=1:row
         for j=1:colum
+        %% اگر مجموع i,j بر 2 بخش پذیر باشد پیکسل صفر و در غیر این صورت پیکسل مقدار یکرا دریافت می کند.
             if mod((i+j),2)==0
                 image(i,j)=0;
             else  
