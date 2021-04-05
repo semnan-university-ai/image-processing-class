165bd6b8c0710f67003abd231508ed3c6f063409
mahlamahdi <56906182+mahlamahdi@users.noreply.github.com>
2021-04-05 21:51:55 +0430

    Add files via upload

---
+++ excersiecs/mahlamahdi/3/ex03/ex03.m
@@ -0,0 +1,12 @@
+clc;
+clear;
+close all;
+img1 = imread("img/airplane.png");
+imagesize=size(img1);
+     for i = 1:imagesize(1)
+        for j=1:imagesize(2)
+            img2(i,j) = 0.33*(img1(i, j, 1)) + 0.33*(img1(i, j, 2)) + 0.33*(img1(i, j, 3));
+        end
+     end
+    subplot(1,2,1),imshow(img1);
+    subplot(1,2,2),imshow(img2);
