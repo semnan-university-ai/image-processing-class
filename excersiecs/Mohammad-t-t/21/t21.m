79e6ac2dd8bd3725c56367c666da0399df578b86
Mohammad-t-t <79921327+Mohammad-t-t@users.noreply.github.com>
2021-04-06 19:54:46 +0430

    Update t21.m

--- excersiecs/Mohammad-t-t/21/t21.m
+++ excersiecs/Mohammad-t-t/21/t21.m
@@ -2,16 +2,12 @@ clc;
 clear;
 close all;
 
-image = imread('monarch.png');
+image = imread('girl.png');
 sizePic= size(image);
 result=image;
 
-for j=1 : sizePic(1)
-    for i=1 : sizePic(2)
-        for k = 1 : 3
-            result(j,i,k)=fix(image(j,i,k)/51)*51;
-        end
-    end
+for k =1 :3
+result(:,:,k)=fix(image(:,:,k)/51)*51;
 end
 
 subplot(1,2,1); imshow(image);
