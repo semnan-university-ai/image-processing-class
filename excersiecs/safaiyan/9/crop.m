ee0e038017ba855386f0b4d2a3079194b3518408
safaiyan <79852777+safaiyan@users.noreply.github.com>
2021-04-06 16:14:10 +0430

    tamrin 9

---
+++ excersiecs/safaiyan/9/crop.m
@@ -0,0 +1,12 @@
+clc;
+close all;
+clear all;
+
+img=imread('C:\Users\Nakisa™\Documents\GitHub\image-processing-class\benchmark\girl.png');
+row1=input('please insert row1');
+row2=input('please insert row2');
+col1=input('please insert col1');
+col2=input('please insert col2');
+subplot(1,2,1),imshow(img);
+subImage = img(row1:row2, col1:col2, :);
+subplot(2,2,4),imshow(subImage);
