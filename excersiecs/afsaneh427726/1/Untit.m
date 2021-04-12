df81826d4183ed7eac7d48b6378027966d459fa4
afsaneh427726 <79640096+afsaneh427726@users.noreply.github.com>
2021-04-12 23:32:32 +0430

    Add files via upload

---
+++ excersiecs/afsaneh427726/1/Untit.m
@@ -0,0 +1,27 @@
+clc;
+clear data;
+clear all;
+b=input('tool ra vared kon :   ')
+a=input('arz ra vared kon :   ')
+c=input('andazeye morabbaha vared kon :   ')
+w=ones(a*c,b*c);
+
+for i=1:a*c
+    if (mod(i,2*c)==1)
+        for j=1:b*c
+            if (mod(j,2*c)==1)
+            w(i:(i+(c-1)),j:(j+(c-1)))=0;
+            end
+        end
+    end
+end
+for i=2:a*c
+    if (mod((i),(2*c))==(c+1))
+        for j=2:b*c
+            if (mod(j,(2*c))==(c+1))
+            w(i:(i+(c-1)),j:(j+(c-1)))=0;
+            end
+        end
+    end
+end
+ imshow(w)
