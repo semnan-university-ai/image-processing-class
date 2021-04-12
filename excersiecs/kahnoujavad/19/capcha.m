715e33efb360b6bb39bc4ae1b1a78bc78832cdc8
javad.kahnouji <79843262+kahnoujavad@users.noreply.github.com>
2021-04-12 04:44:27 -0700

    tamrin 16-19

---
+++ excersiecs/kahnoujavad/19/capcha.m
@@ -0,0 +1,33 @@
+clc;
+clear all;
+close all;
+img=imread('A-Z');
+imgcap=zeros(0,0,'logical');
+a1=0;
+c='';
+for i=0:8
+   
+    L=randi([0,3]);
+    if L<2 
+        ttl=65+(L*13+B);
+    else
+        ttl=71+(L*13+B);
+    end
+    
+    code=strcat(code,char(ttl));
+    p=randi([1,20]);
+    q=randi([1,10]);
+    
+    C=B*100+1;
+    D=i*40+1+q;
+    h=L*100+1;
+    imgcaptcha(p:59+p, D:D+39) = img(40+h:99+h, C+30:C+69);
+end    
+%add noise
+for i=1:7000
+    X=randi([1,size(imgcaptcha,1)]);
+    Y=randi([1,size(imgcaptcha,2)]);
+    imgcaptcha(X,Y)=randi([0,1])*255;
+end
+figure;
+imshow(imgcaptcha),title(code);
