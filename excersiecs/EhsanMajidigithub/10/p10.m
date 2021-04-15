a0b7f2cdac9d2241e6d55108cda89932f1cf5dc5
EhsanMajidigithub <79828266+EhsanMajidigithub@users.noreply.github.com>
2021-04-15 15:34:53 +0430

    Update p10.m

--- excersiecs/EhsanMajidigithub/10/p10.m
+++ excersiecs/EhsanMajidigithub/10/p10.m
@@ -1,19 +1,21 @@
-close all
-clear all
-clc
-pic=zeros(100,400);
-pic(1:100,100:100:400)=1;
-for i=1:4
-   d=randi(10);
-   add="images\numbers\"+int2str(d-1)+".png";
-   a=imread(add);
-   for r=1:100
-       for k=1:100
-           c=(i-1)*100+k;
-           pic(r,c)=(255-a(r,k));
-       end
-   end    
-end 
+clc;clear;close all;
+
+img=imread('0-9');
+imgcaptcha=zeros(0,0,'logical');
+ttl=0;
+for i=0:3
+    B=randi([1,9]);
+    ttl=ttl+B*10^(3-i);
+    p=randi([1,20]);
+    q=randi([1,10]);
+    C=B*100+1;
+    D=i*40+1+q;
+    imgcaptcha(p:60+p, D:D+39) = img(40:100, C+30:C+69);
+end    
+for i=1:3000
+    X=randi([1,size(imgcaptcha,1)]);
+    Y=randi([1,size(imgcaptcha,2)]);
+    imgcaptcha(X,Y)=randi([0,1])*255;
+end
 figure;
-pic=im2bw(pic);
-imshow(pic)
+imshow(imgcaptcha),title(int2str(ttl));
