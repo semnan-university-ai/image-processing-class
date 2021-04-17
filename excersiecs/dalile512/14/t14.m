4d4689d7e74551c6cf40c908dde1d25a6d339ea5
dalile512 <80260988+dalile512@users.noreply.github.com>
2021-04-17 10:36:46 +0430

    Update t14.m

--- excersiecs/dalile512/14/t14.m
+++ excersiecs/dalile512/14/t14.m
@@ -5,7 +5,7 @@ clear all;
 pictures =dir('a/*.jpg');
 
 for k =1:9
-        pic=imageList(k).name;
+        pic=pictures(k).name;
         image=imread(pic);
         image=rgb2gray(image);
         subplot(3,3,k)
