4f563278278253fd074ec7e9a66eb3b496281e8f
Mohammad-t-t <79921327+Mohammad-t-t@users.noreply.github.com>
2021-04-19 06:07:08 +0430

    Update t13.m

--- excersiecs/Mohammad-t-t/13/t13.m
+++ excersiecs/Mohammad-t-t/13/t13.m
@@ -3,39 +3,35 @@ clear;
 close all;
 
 orginalPic=imread('girl.png');
-if size(size(orginalPic))==3
+
+x=size(size(orginalPic),2);
+if x==3
     grayPic=rgb2gray(orginalPic);
-    picSize=size(grayPic);
 else
     grayPic=orginalPic;
-    picSize=size(orginalPic);
 end
 
+picSize=size(grayPic);
 
 %negativePic
-
-for x =1: picSize(2)
-    for y =1 : picSize(1)
-        negativePic(y,x)=255-grayPic(y,x);
-    end
-end
+negativePic = 255 - grayPic;
 
 %noise
 tedad=4;
-y=0;
 for i=1 : tedad
-noise=randi([1,20]);
-x = randi([0,255],picSize(1),picSize(2));
-negativePic(x <= noise+0) = 0;  
-negativePic(x >=255-noise) = 255;
-averagesum=0;
-    for m=1 : picSize(2)
-        for n=1: picSize(1)
-           averagesum=sum(sum(negativePic));
-            %y=y+temp;
-        end
-    end
-    average(i)=averagesum/(n*m);
-    figure, imshow(negativePic);
-end
+noisPic=negativePic;
+noise=randi([1,30]);
 
+x = randi([0,255],picSize(1),picSize(2));
+noisPic(x <= noise+0) = 0;  
+noisPic(x >=255-noise) = 255;
+average(i)=sum(noisPic(:))/(picSize(1)*picSize(2));
+
+    subplot(2,3,i);
+    imshow(noisPic),title(average(i));
+end
+    
+    subplot(2,3,tedad+1);
+    plot(average);
+    
+xlswrite('average.xls',average);
