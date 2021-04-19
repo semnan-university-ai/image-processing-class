683fe022c12a86b080315f1e115bd8829dfbe8c4
FatemehSeyfi <80269176+FatemehSeyfi@users.noreply.github.com>
2021-04-19 00:08:58 -0700

    Update q10.m

--- excersiecs/FatemehSeyfi/10/q10.m
+++ excersiecs/FatemehSeyfi/10/q10.m
@@ -7,9 +7,9 @@ for i=1:4
   j=randi(10);
    inc="numbers\"+int2str(j-1)+".png";
    b=imread(inc);
-   for m=1:99
-       for k=1:99
-           c=(i-1)*99+k;
+   for m=1:100
+       for k=1:100
+           c=(i-1)*100+k;
            image(m,c)=(255-b(m,k));
        end
    end    
