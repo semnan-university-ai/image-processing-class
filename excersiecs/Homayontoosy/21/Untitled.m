c1f8763f6e83adf8ce4c9500b1930c5e666821a8
unknown <homayon.toosy@gmail.com>
2021-04-28 16:50:30 +0430

    part 2 tamarin
    tamrin21

---
+++ excersiecs/Homayontoosy/21/Untitled.m
@@ -0,0 +1,32 @@
+clear all
+clc
+clear;
+
+image=imread('D:\benchmark\girl.png');   
+size=size(image);             
+for i=1:size(1)               
+    for j=1:size(2)
+        for k=1:size(3)
+        if image(i,j,k)<=50
+            image(i,j,k)=0;
+        end
+        if (image(i,j,k)>=51)&&(image(i,j,k)<=101)
+                image(i,j,k) = 51;
+        end
+        if (image(i,j,k)>=102)&&(image(i,j,k)<=152)
+                image(i,j,k) = 102;
+        end
+        if (image(i,j,k)>=153)&&(image(i,j,k)<=203)
+                image(i,j,k) = 153;
+        end
+        if (image(i,j,k)>=204)&&(image(i,j,k)<=254)
+                image(i,j,k) = 204;
+        end
+            if (image(i,j,k)==255)
+                image(i,j,k) = 255;
+            end
+        end
+    end
+end
+image=uint8(image);   
+imshow(image)
