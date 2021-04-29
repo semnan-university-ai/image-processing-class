c1e3834302abab4f0cf148b9e011a8b4c7f30259
omidgholami74 <48456571+omidgholami74@users.noreply.github.com>
2021-04-29 11:32:03 +0430

    Update e21.m

--- excersiecs/omidgholami74/21/e21.m
+++ excersiecs/omidgholami74/21/e21.m
@@ -1,7 +1,10 @@
-clc;
-close all;
-clear all;
-
-image =imread('benchmark/sails.png');
-
-imshow(image);
+pic=imread('benchmark/sails.png');
+figure;imshow(pic);
+[height,width,k]=size(pic);
+for i=1:height
+    for j=1:width
+            pic(i,j,:)=floor(pic(i,j,:)/51)*51;
+    end
+end 
+pic=uint8(pic);
+figure;imshow(pic);
