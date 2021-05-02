94568627357a60fab2465949496015428ab82d2b
nastaranm57 <79742080+nastaranm57@users.noreply.github.com>
2021-05-03 02:49:05 +0430

    Add files via upload

---
+++ excersiecs/nastaranm57/24/24.m
@@ -0,0 +1,19 @@
+pool=imread('C:\Users\PC\Desktop\pool.png');
+pool=imresize(pool,[768 1024]);
+subplot(2,2,1)
+imshow(pool);
+title('original pool');
+watch=imread('C:\Users\PC\Desktop\watch.png');
+subplot(2,2,3)
+imshow(watch);
+title('original watch');
+pg=pool(:,:,1);
+wg= watch(:,:,1);
+watch(:,:,1)=pg;
+pool(:,:,1)=wg;
+subplot(2,2,2)
+imshow(pool);
+title('changed pool');
+subplot(2,2,4)
+imshow(watch);
+title('changed watch');
