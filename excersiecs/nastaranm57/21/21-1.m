977611ebfc844a3a0101985f17f78ff19a546e32
nastaranm57 <79742080+nastaranm57@users.noreply.github.com>
2021-05-03 01:53:05 +0430

    Update 21-1.m

--- excersiecs/nastaranm57/21/21-1.m
+++ excersiecs/nastaranm57/21/21-1.m
@@ -2,7 +2,8 @@ colorValues = (0:0.199:1).';  %'
 webSafeMap = [repmat(colorValues,36,1) ...
               kron(colorValues,ones(36,1)) ...
               repmat(kron(colorValues,ones(6,1)),6,1)];
-imageRGB = imread('C:\Users\PC\Desktop\cat.png');  %# Load a built-in image
+addpath("../../../benchmark/");
+imageRGB = imread("cat.png"); 
 imageRGB2 = ind2rgb(rgb2ind(imageRGB,webSafeMap),webSafeMap);
 subplot(1,2,1);imshow(imageRGB);
 title('Original Image');
