bc3a2a003ce94c3e9661f693f13a29fc4e9c8a7b
nastaranm57 <79742080+nastaranm57@users.noreply.github.com>
2021-05-03 11:34:27 +0430

    Add files via upload

---
+++ excersiecs/nastaranm57/23/e23.m
@@ -0,0 +1,13 @@
+clc;
+close all;
+clear all;
+
+pic=imread('C:\Users\PC\Desktop\lena.png');
+
+subplot(2,2,1);imshow(pic);title('original image')
+hisR=his(pic,1);
+hisG=his(pic,2);
+hisB=his(pic,3);
+ subplot(2,2,2);bar(hisR);title('Red')
+ subplot(2,2,3);bar(hisG);title('Green')
+ subplot(2,2,4);bar(hisB);title('Blue')
